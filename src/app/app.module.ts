23cb6af6eaacfaa8a365e70bec8cc9980e7d66d8
Sergey <ponchikpk@yandex.ru>
2019-09-25 12:40:39 +0300

    four components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {OneComponent} from './one/one.component';
+import {TwoComponent} from './two/two.component';
+import {TreeComponent} from './tree/tree.component';
+import {FourComponent} from './four/four.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        OneComponent,
+        TwoComponent,
+        TreeComponent,
+        FourComponent,
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/four/four.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-four',
+  templateUrl: './four.component.html',
+  styleUrls: ['./four.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class FourComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
